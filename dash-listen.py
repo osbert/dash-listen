feb98a98180c75a85914868979cf9011e996519b
Osbert Feng <osbert@iterinc.com>
2015-09-27 11:49:25 -0700

    Dockerize, use DASH_MAC_ADDRESS and URL_CALLBACK

--- dash-listen.py
+++ dash-listen.py
@@ -1,13 +1,26 @@
-from scapy.all import *
-
-def arp_display(pkt):
-  if pkt[ARP].op == 1: #who-has (request)
-    if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
-      print "ARP Probe from: " + pkt[ARP].hwsrc
-
-print sniff(prn=arp_display, filter="arp", store=0, count=10)
-
 # sudo apt-get install python-scapy tcpdump tcpreplay wireshark
 
 # Note that wireshark prompts during installation if non-root users
 # should be allowed to perform packed capture.
+
+from scapy.all import *
+import os
+import requests
+
+DASH_MAC_ADDRESS=os.environ.get('DASH_MAC_ADDRESS')
+URL_CALLBACK=os.environ.get('URL_CALLBACK')
+
+def arp_display(pkt):
+  if pkt[ARP].op == 1: #who-has (request)
+    if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
+      if pkt[ARP].hwsrc == DASH_MAC_ADDRESS:
+        requests.get(URL_CALLBACK)
+      else:
+        print "ARP Probe from unknown device: " + pkt[ARP].hwsrc
+
+
+while True:
+    try:
+        print sniff(prn=arp_display, filter="arp", store=0, count=10)
+    except:
+        pass
