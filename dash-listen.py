aaa17a4bb10ec8de9d9d6e495cb89143c46fe35f
Osbert Feng <osbert@iterinc.com>
2015-09-27 11:43:38 -0700

    Initial dash listen script.

---
+++ dash-listen.py
@@ -0,0 +1,13 @@
+from scapy.all import *
+
+def arp_display(pkt):
+  if pkt[ARP].op == 1: #who-has (request)
+    if pkt[ARP].psrc == '0.0.0.0': # ARP Probe
+      print "ARP Probe from: " + pkt[ARP].hwsrc
+
+print sniff(prn=arp_display, filter="arp", store=0, count=10)
+
+# sudo apt-get install python-scapy tcpdump tcpreplay wireshark
+
+# Note that wireshark prompts during installation if non-root users
+# should be allowed to perform packed capture.
